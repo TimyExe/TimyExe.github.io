0efc8cfbf7ca54708c57d162e9b42b9db984384f
TimyExe <94359572+TimyExe@users.noreply.github.com>
2023-08-21 11:17:37 +0200

    Update success.js

--- success.js
+++ success.js
@@ -1,5 +1,5 @@
-// Check if the user is logged in
-const isLoggedIn = /* Check if user is logged in */ true;
+// Check if the user is logged in (simulated using a cookie)
+const isLoggedIn = document.cookie.includes('sessionToken');
 
 // Get references to elements
 const overlay = document.getElementById('overlay');
@@ -9,13 +9,17 @@ const pdfContainer = document.querySelector('.pdf-container');
 // Show the overlay if logged in
 if (isLoggedIn) {
     overlay.style.display = 'block';
+    pdfContainer.style.display = 'block'; // Show the PDF container if logged in
 } else {
+    overlay.style.display = 'none'; // Hide the overlay if not logged in
     pdfContainer.style.display = 'none'; // Hide the PDF container if not logged in
 }
 
 // Handle logout button click
 logoutButton.addEventListener('click', () => {
-    // Perform logout actions if needed
-    // For this example, let's redirect back to the login page
+    // Clear the session token cookie
+    document.cookie = 'sessionToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+
+    // Redirect to the login page
     window.location.href = 'index.html';
 });
