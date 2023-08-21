8d38c35546c6166fc196497e2c0d4fceecb61173
TimyExe <94359572+TimyExe@users.noreply.github.com>
2023-08-21 10:34:38 +0200

    Update success.js

--- success.js
+++ success.js
@@ -4,10 +4,13 @@ const isLoggedIn = /* Check if user is logged in */ true;
 // Get references to elements
 const overlay = document.getElementById('overlay');
 const logoutButton = document.getElementById('logoutButton');
+const pdfContainer = document.querySelector('.pdf-container');
 
 // Show the overlay if logged in
 if (isLoggedIn) {
     overlay.style.display = 'block';
+} else {
+    pdfContainer.style.display = 'none'; // Hide the PDF container if not logged in
 }
 
 // Handle logout button click
