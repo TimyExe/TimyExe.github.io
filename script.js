42478db3518e17a4b71b867435049e3d8fbdb5d1
TimyExe <94359572+TimyExe@users.noreply.github.com>
2023-08-21 14:09:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
     loadPdf1Button.addEventListener("click", function() {
         pdfContainer.innerHTML = `<iframe src="9473128851463894736473964732985736437571246713941237539846374983126478129.pdf" class="pdf-iframe" frameborder="0"></iframe>`;
-    });
+        pdfContainer.classList.add("active");
+     });
 
     loadPdf2Button.addEventListener("click", function() {
         pdfContainer.innerHTML = `<iframe src="8321658250125357385934724301243817438567105836409371847317563.pdf" class="pdf-iframe" frameborder="0"></iframe>`;
-    });
+        pdfContainer.classList.add("active");
+      });
 });
