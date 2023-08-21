5d632f185f87b3c2fbec96916f5cd78a1e42ec06
TimyExe <94359572+TimyExe@users.noreply.github.com>
2023-08-21 12:09:38 +0200

    Update login.js

--- login.js
+++ login.js
@@ -5,9 +5,21 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
     const password = document.getElementById('password').value;
 
     // Simulated authentication (replace this with actual authentication logic)
-    if (username === 'one' && password === 'two') {
+    if (username === 'user1' && password === 'password1') {
         // Set a session token cookie
-        document.cookie = 'sessionToken=yourSessionToken; path=/;';
+        document.cookie = 'sessionToken=user1Token; path=/;';
+
+        // Redirect to the success page
+        window.location.href = 'success.html';
+    } else if (username === 'user2' && password === 'password2') {
+        // Set a session token cookie
+        document.cookie = 'sessionToken=user2Token; path=/;';
+
+        // Redirect to the success page
+        window.location.href = 'success.html';
+    } else if (username === 'user3' && password === 'password3') {
+        // Set a session token cookie
+        document.cookie = 'sessionToken=user3Token; path=/;';
 
         // Redirect to the success page
         window.location.href = 'success.html';
