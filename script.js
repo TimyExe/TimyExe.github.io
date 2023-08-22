533adf150302998df83e409d6e7b3a1a77b0cd5a
TimyExe <94359572+TimyExe@users.noreply.github.com>
2023-08-22 08:55:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const pdfContainer = document.querySelector(".pdf-iframe-container");
-    const loadPdf1Button = document.getElementById("loadPdf1");
-    const loadPdf2Button = document.getElementById("loadPdf2");
+    const cards = document.querySelectorAll(".card");
+    const pdfIframeContainer = document.getElementById("pdfIframeContainer");
+    const backButton = document.getElementById("backButton");
 
-    loadPdf1Button.addEventListener("click", function() {
-        pdfContainer.innerHTML = `<iframe src="9473128851463894736473964732985736437571246713941237539846374983126478129.pdf" class="pdf-iframe" frameborder="0"></iframe>`;
-        pdfContainer.classList.add("active");
-     });
+    const pdfPaths = {
+        card1: "9473128851463894736473964732985736437571246713941237539846374983126478129.pdf",
+        card2: "8321658250125357385934724301243817438567105836409371847317563.pdf",
+        card3: "path/to/pdf3.pdf"
+    };
 
-    loadPdf2Button.addEventListener("click", function() {
-        pdfContainer.innerHTML = `<iframe src="8321658250125357385934724301243817438567105836409371847317563.pdf" class="pdf-iframe" frameborder="0"></iframe>`;
-        pdfContainer.classList.add("active");
-      });
+    cards.forEach(card => {
+        card.addEventListener("click", function() {
+            const cardId = card.id;
+            const pdfSrc = pdfPaths[cardId];
+            if (pdfSrc) {
+                pdfIframeContainer.innerHTML = `<iframe src="${pdfSrc}" frameborder="0"></iframe>`;
+                pdfIframeContainer.classList.add("active");
+            }
+        });
+    });
+
+    backButton.addEventListener("click", function() {
+        pdfIframeContainer.innerHTML = "";
+        pdfIframeContainer.classList.remove("active");
+    });
 });
