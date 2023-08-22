a522cf91088becab98e9bde32d0427ee59a78a9e
TimyExe <94359572+TimyExe@users.noreply.github.com>
2023-08-22 12:42:38 +0200

    Update success.js

--- success.js
+++ success.js
@@ -1,30 +1,10 @@
-// Check if the user is logged in (simulated using a cookie)
-const isLoggedIn = document.cookie.includes('sessionToken');
+document.addEventListener("DOMContentLoaded", function() {
+    // Check if the user is logged in (simulated using a cookie)
+    const isLoggedIn = document.cookie.includes("sessionToken");
 
-// Redirect to login page if not logged in
-if (!isLoggedIn) {
-    window.location.href = 'index.html'; // Redirect to the login page
-}
+    if (!isLoggedIn) {
+        window.location.href = "index.html"; // Redirect to the login page
+    }
 
-// Get references to elements
-const overlay = document.getElementById('overlay');
-const logoutButton = document.getElementById('logoutButton');
-const pdfContainer = document.querySelector('.pdf-container');
-
-// Show the overlay if logged in
-if (isLoggedIn) {
-    overlay.style.display = 'block';
-    pdfContainer.style.display = 'block'; // Show the PDF container if logged in
-} else {
-    overlay.style.display = 'none'; // Hide the overlay if not logged in
-    pdfContainer.style.display = 'none'; // Hide the PDF container if not logged in
-}
-
-// Handle logout button click
-logoutButton.addEventListener('click', () => {
-    // Clear the session token cookie
-    document.cookie = 'sessionToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-
-    // Redirect to the login page
-    window.location.href = 'index.html';
+    // Rest of the cards.js script (PDF card viewer functionality)
 });
