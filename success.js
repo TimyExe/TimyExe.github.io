ba2cfc077aa64cf2862c841d36e75cbdafb39629
TimyExe <94359572+TimyExe@users.noreply.github.com>
2023-08-22 12:44:01 +0200

    Update success.js

--- success.js
+++ success.js
@@ -6,5 +6,43 @@ document.addEventListener("DOMContentLoaded", function() {
         window.location.href = "index.html"; // Redirect to the login page
     }
 
-    // Rest of the cards.js script (PDF card viewer functionality)
+    const cardsContainer = document.querySelector(".cards-container");
+    const pdfContainer = document.querySelector(".pdf-container");
+    const pdfIframe = document.getElementById("pdfIframe");
+    const backButton = document.getElementById("backButton");
+
+    const pdfPaths = [
+        "path/to/pdf1.pdf",
+        "path/to/pdf2.pdf",
+        "path/to/pdf3.pdf"
+        // Add more PDF paths as needed
+    ];
+
+    function createCard(imgSrc, pdfSrc) {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        const img = document.createElement("img");
+        img.src = imgSrc;
+        img.alt = "Card Image";
+        card.appendChild(img);
+
+        card.addEventListener("click", function() {
+            pdfIframe.src = pdfSrc;
+            pdfContainer.classList.add("active");
+            backButton.style.display = "block";
+        });
+
+        return card;
+    }
+
+    pdfContainer.addEventListener("click", function() {
+        pdfIframe.src = "";
+        pdfContainer.classList.remove("active");
+        backButton.style.display = "none";
+    });
+
+    for (let i = 1; i <= pdfPaths.length; i++) {
+        const card = createCard(`icon${i}.jpg`, pdfPaths[i - 1]);
+        cardsContainer.appendChild(card);
+    }
 });
