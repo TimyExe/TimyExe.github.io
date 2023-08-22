de60be402830c01b12bff2291a25073c354d5be9
TimyExe <94359572+TimyExe@users.noreply.github.com>
2023-08-22 09:13:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     const cards = document.querySelectorAll(".card");
     const pdfIframeContainer = document.getElementById("pdfIframeContainer");
+    const pdfIframe = document.getElementById("pdfIframe");
     const backButton = document.getElementById("backButton");
 
     const pdfPaths = {
         card1: "9473128851463894736473964732985736437571246713941237539846374983126478129.pdf",
         card2: "8321658250125357385934724301243817438567105836409371847317563.pdf",
-        card3: "path/to/pdf3.pdf"
+        card3: "path/to/your_third_pdf.pdf"  // Replace with the actual path
     };
 
     cards.forEach(card => {
@@ -14,14 +15,14 @@ document.addEventListener("DOMContentLoaded", function() {
             const cardId = card.id;
             const pdfSrc = pdfPaths[cardId];
             if (pdfSrc) {
-                pdfIframeContainer.innerHTML = `<iframe src="${pdfSrc}" frameborder="0"></iframe>`;
+                pdfIframe.src = pdfSrc;
                 pdfIframeContainer.classList.add("active");
             }
         });
     });
 
     backButton.addEventListener("click", function() {
-        pdfIframeContainer.innerHTML = "";
+        pdfIframe.src = "";
         pdfIframeContainer.classList.remove("active");
     });
 });
