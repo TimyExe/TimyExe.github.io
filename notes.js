e903b7fb17072adb89e4529f2a0e67632ef388c7
TimyExe <tim55kat@gmail.com>
2023-08-25 12:36:15 +0200

    dededede

--- notes.js
+++ notes.js
@@ -1,35 +1,65 @@
-const textList = [
-    '\txts\cancer.txt',
-    'text2.txt',
-    'text3.txt'
-];
-
-const textListContainer = document.querySelector('.text-list');
-const textOverlay = document.querySelector('.text-overlay');
-const textContent = document.querySelector('.text-content');
-const backButton = document.querySelector('.back-button');
-
-// Load text bars
-textList.forEach((text, index) => {
-    const textBar = document.createElement('div');
-    textBar.classList.add('text-bar');
-    textBar.textContent = `Text ${index + 1}`;
-    textBar.addEventListener('click', () => openTextOverlay(text));
-    textListContainer.appendChild(textBar);
-});
-
-// Open text overlay
-function openTextOverlay(text) {
-    fetch(text)
-        .then(response => response.text())
-        .then(content => {
-            textContent.textContent = content;
-            textOverlay.style.display = 'flex';
-        });
+// Function to fetch a text file
+async function fetchTextFile(filename) {
+    const response = await fetch(filename);
+    const text = await response.text();
+    return text;
 }
 
-// Close text overlay
-backButton.addEventListener('click', () => {
-    textOverlay.style.display = 'none';
+// Function to create a text button
+function createTextButton(textFileName) {
+    const button = document.createElement('button');
+    button.textContent = textFileName;
+    button.addEventListener('click', () => loadTextContent(textFileName));
+    return button;
+}
+
+// Function to display text content in the overlay
+function loadTextContent(textFileName) {
+    const overlay = document.querySelector('.overlay');
+    const textContainer = document.querySelector('.text-container');
+    const backButton = document.getElementById('backButton');
+
+    const textContent = fetchTextFile(textFileName);
+
+    textContent.then((text) => {
+        textContainer.innerHTML = `<pre>${text}</pre>`;
+        overlay.classList.add('active');
+        textContainer.classList.add('active');
+        backButton.style.display = 'block'; // Show the back button
+    });
+}
+
+// Get references to elements
+const logoutButton = document.getElementById('logoutButton');
+const textButtonsContainer = document.querySelector('.text-buttons');
+const overlay = document.querySelector('.overlay');
+const backButton = document.getElementById('backButton');
+const searchInput = document.getElementById('searchInput');
+const textContainer = document.querySelector('.text-container');
+
+// Load text buttons
+const textFiles = [
+    "txts/cancer.txt",
+    "file2.txt",
+    "file3.txt"
+];
+
+textFiles.forEach((textFile) => {
+    const textButton = createTextButton(textFile);
+    textButtonsContainer.appendChild(textButton);
 });
 
+// Handle logout button click
+logoutButton.addEventListener('click', () => {
+    // Clear the session token cookie
+    document.cookie = 'sessionToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    // Redirect to the login page
+    window.location.href = 'index.html';
+});
+
+// Back button functionality
+backButton.addEventListener('click', () => {
+    overlay.classList.remove('active');
+    textContainer.classList.remove('active');
+    backButton.style.display = 'none';
+});
