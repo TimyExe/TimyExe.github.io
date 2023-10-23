d2f95bdae23ae9b5f9734faf336ed6218fe8643a
TimyExe <94359572+TimyExe@users.noreply.github.com>
2023-10-23 14:11:08 +0200

    Update check.js

--- check.js
+++ check.js
@@ -1,1 +1,7 @@
-dd
+document.addEventListener("DOMContentLoaded", function() {
+    // Check if the user is logged in (simulated using a cookie)
+    const isLoggedIn = document.cookie.includes("sessionToken");
+
+    if (!isLoggedIn) {
+        window.location.href = "index.html"; // Redirect to the login page
+    }
