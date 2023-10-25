de0e9cb74eabe566ef2e3bea9541eb84c7509016
TimyExe <tim55kat@gmail.com>
2023-10-25 11:31:20 +0200

    bismillah

--- mat-exam/README.md
+++
@@ -1,1 +0,0 @@
-# TimyExe.github.io
--- mat-exam/check.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Check if the user is logged in (simulated using a cookie)
-    const isLoggedIn = document.cookie.includes("sessionToken");
-
-    if (!isLoggedIn) {
-        window.location.href = "index.html"; // Redirect to the login page
-    }
-});
