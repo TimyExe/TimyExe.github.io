7ad424984366804643c22657a02daf9ed1eee000
TimyExe <tim55kat@gmail.com>
2023-10-27 10:30:31 +0200

    firebase 5 hours

--- login.js
+++ login.js
@@ -1,18 +1,19 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyDkIw-MiE9VB9VaJ2EMNYV6ukM8gvw0UWY",
-    authDomain: "hookey-2beee.firebaseapp.com",
-    databaseURL: "https://hookey-2beee-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "hookey-2beee",
-    storageBucket: "hookey-2beee.appspot.com",
-    messagingSenderId: "873667938609",
-    appId: "1:873667938609:web:3f7cf35ad219ea8d018810"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  // Initialize variables
-  const auth = firebase.auth()
-  const database = firebase.database()
+  apiKey: "AIzaSyDkIw-MiE9VB9VaJ2EMNYV6ukM8gvw0UWY",
+  authDomain: "hookey-2beee.firebaseapp.com",
+  databaseURL: "https://hookey-2beee-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "hookey-2beee",
+  storageBucket: "hookey-2beee.appspot.com",
+  messagingSenderId: "873667938609",
+  appId: "1:873667938609:web:3f7cf35ad219ea8d018810"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Get a reference to the Firebase Realtime Database
+const database = getDatabase(app);
   
   // Set up our register function
   function register () {
