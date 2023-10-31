55dc121118d8e8b5ebe6ad8009f02f54b6e18072
Akki <93147274+2Akki@users.noreply.github.com>
2023-10-31 15:45:35 +0100

    Update success.js

--- success/success.js
+++ success/success.js
@@ -1,10 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Check if the user is logged in (simulated using a cookie)
-    const isLoggedIn = document.cookie.includes("sessionToken");
-
-    if (!isLoggedIn) {
-        window.location.href = "index.html"; // Redirect to the login page
-    }
+   
 
     const cardsContainer = document.querySelector(".cards-container");
     const pdfContainer = document.querySelector(".pdf-container");
